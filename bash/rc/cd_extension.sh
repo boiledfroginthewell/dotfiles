00ae445566dd050be2297418ceda309afe411ec3
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2019-04-19 12:45:57 +0900

    Add cd extensions.
    Add fasd, zp, z., zc, cl commands.
    Modify l and calc so that they work in macOS.

---
+++ bash/rc/cd_extension.sh
@@ -0,0 +1,56 @@
+# ls after cd
+cl () {
+    \cd "$@" && ls
+}
+alias cd=cl
+
+
+# Install fasd
+type fasd > /dev/null && eval "$(fasd --init auto)"
+
+# cd toward parent directories
+zp() {
+	IFS_BK="$IFS"
+	IFS=/
+	dirs=( $(pwd) )
+	IFS="$IFS_BK"
+
+	dest=""
+	for x in "${dirs[@]}"; do
+		dest="${dest}/$x"
+		if [[ "$x" == *"$1"* ]]; then
+			shift
+		fi
+
+		if [ $# = 0 ];then
+			break
+		fi
+	done
+
+	if [ $# != 0 ];then
+		return
+	fi
+
+	cd "$dest"
+}
+
+# "cd ../" multiple times
+__z_multi_dots() {
+	if [ $# = 0 ];then
+		return
+	fi
+
+	echo $1
+	pat="^[0-9]+$"
+	if [[ ! $1 =~ $pat ]]; then
+		return
+	fi
+
+	cd $(seq -s "" -f "../" $1)
+}
+alias z.=__z_multi_dots
+
+
+alias zc='z "$(pwd)" "$@"'
+
+
