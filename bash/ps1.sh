12acbca411d83798927de7d7e741530956a2cf35
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2019-11-18 12:35:55 +0900

    Windows diff.

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -12,7 +12,7 @@ esac
 # uncomment for a colored prompt, if the terminal has the capability; turned
 # off by default to not distract the user: the focus in a terminal window
 # should be on the output of commands, not on the prompt
-#force_color_prompt=yes
+force_color_prompt=yes
 
 if [ -n "$force_color_prompt" ]; then
     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
@@ -21,7 +21,7 @@ if [ -n "$force_color_prompt" ]; then
 	# a case would tend to support setf rather than setaf.)
 	color_prompt=yes
     else
-	color_prompt=
+	color_prompt=yes
     fi
 fi
 
@@ -50,4 +50,3 @@ xterm*|rxvt*)
 *)
     ;;
 esac
-
