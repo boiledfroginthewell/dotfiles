da79e9599fb6f7be88dd958e198141d62d8fd581
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2019-12-02 10:48:23 +0900

    Remove LESSPIPE config.

--- bash/rc/less.sh
+++ bash/rc/less.sh
@@ -4,4 +4,4 @@ export VISUAL="vim"
 
 # syntax highlighting
 export LESS='-R'
-export LESSOPEN='| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
+#export LESSOPEN='| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
