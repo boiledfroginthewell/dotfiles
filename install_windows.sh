7fe65b974a72aa454013f91c2a8a6f50c2af930a
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-01-29 17:01:16 +0900

    Fix Windows installation.

--- install_windows.sh
+++ install_windows.sh
@@ -1,8 +1,13 @@
-CONF_DIR=$HOME/cfg/dotfiles
-
-echo "Configuring XDG_CONFIG_HOME ..."
-< "$CONF_DIR/bash/bashrc" sed -i 's/export XDG_CONFIG_HOME=.*$/export XDG_CONFIG_HOME="$HOME/cfg/dotfiles"'
+CONF_DIR=$HOME/config/dotfiles
 
 echo "Replacing git user information by ***REMOVED***..."
 < "$CONF_DIR/git/config" sed -i -e 's/boiledfroginthewell@users\.noreply\.github\.com/***REMOVED***@***REMOVED***/' -e 's/boiledfroginthewell/***REMOVED***/'
+
+# CMD
+mklink /J "$HOME/.vim" "$CONF_DIR/vim"
+
+# git
+git config --global --add user.name ***REMOVED***
+git config --global --add user.email ***REMOVED***@***REMOVED***
+git config --global --add core.pager less
 
