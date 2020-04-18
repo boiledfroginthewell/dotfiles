877934465060104b5e3246137d62d5ffe6779acd
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-04-18 18:55:04 +0900

    Bash: Enhance fzf configuration.

--- bash/rc/fzf.config.sh
+++ bash/rc/fzf.config.sh
@@ -2,12 +2,12 @@ export FZF_DEFAULT_OPTS="--reverse --multi --cycle --ansi"
 
 KEY_BINDING_FILE=/usr/share/doc/fzf/examples/key-bindings.bash
 if [ -e "$KEY_BINDING_FILE" ]; then
-	source $KEY_BINDING_FILE
+	# Change C-t to C-s
+	source <(< $KEY_BINDING_FILE sed 's/\C-t/\C-s/')
 fi
 
 COMPLETION_FILE=/usr/share/bash-completion/completions/fzf
 if [ -e "$COMPLETION_FILE" ]; then
 	source $COMPLETION_FILE
 fi
-
 
