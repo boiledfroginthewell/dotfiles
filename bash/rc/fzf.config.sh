d4c96536e2286d9ee91995c65c937d8079b38997
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-04-30 08:55:08 +0900

    Bash: Organize bin and others.

--- bash/rc/fzf.config.sh
+++ bash/rc/fzf.config.sh
@@ -5,6 +5,9 @@ if [ -e "$KEY_BINDING_FILE" ]; then
 	# Change C-t to C-s
 	source <(< $KEY_BINDING_FILE sed 's/\C-t/\C-s/')
 fi
+FZF_CTRL_T_OPTS='
+--bind "ctrl-l:execute(l {} > /dev/tty )"
+'
 
 COMPLETION_FILE=/usr/share/bash-completion/completions/fzf
 if [ -e "$COMPLETION_FILE" ]; then
