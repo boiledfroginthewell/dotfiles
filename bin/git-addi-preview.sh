21747a4382efabb898203311cd6bb57d2a6e2dc0
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-04-30 08:55:08 +0900

    Bash: Organize bin and others.

---
+++ bin/git-addi-preview.sh
@@ -0,0 +1,13 @@
+SHOW_DIR_CMD=$(type tree &> /dev/null && echo tree || echo ls)
+
+fileName="${1:3}"
+if [[ "$1" = "?? "* ]]; then
+	if [ -d "$fileName" ]; then
+		$SHOW_DIR_CMD "$fileName"
+	else
+		cat "$fileName"
+	fi
+else
+	git --no-pager diff --color-words -- "$fileName"
+fi
+
