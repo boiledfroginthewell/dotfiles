a105b68698aabeb02bc1a174c75bba864ea0e5b7
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-06-16 10:20:47 +0900

    mac: Configure for Pyenv.

--- bash/rc/pyenv.sh
+++ bash/rc/pyenv.sh
@@ -1,5 +1,8 @@
-export PYENV_ROOT="$HOME/opt/pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+if [[ "$OSTYPE" = "darwin"* ]]; then
+	# GitHub Cloneしてインストールしたときのみ必要. macでは不要
+	export PYENV_ROOT="$HOME/opt/pyenv"
+	export PATH="$PYENV_ROOT/bin:$PATH"
+fi
 
 if which pyenv > /dev/null; then
 	eval "$(pyenv init -)"
