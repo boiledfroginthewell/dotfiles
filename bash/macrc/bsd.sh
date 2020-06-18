d8db745e851df242fd80a74d953f60dc3a4a2c04
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-06-18 10:09:53 +0900

    mac: Fix BSD related config.

--- bash/macrc/bsd.sh
+++ bash/macrc/bsd.sh
@@ -1,3 +1,10 @@
 alias diff="diff --ignore-space-change"
-alias ls="ls -F"
+alias ls="ls -p"
+
+# ls
+export CLICOLOR=1
+export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
+
+# l
+export L_LS_COMMAND='ls -p'
 
--- bash/macrc/ls.sh
+++
@@ -1,4 +0,0 @@
-export CLICOLOR=1
-export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
-# export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
-
