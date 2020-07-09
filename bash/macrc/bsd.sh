a45db34a572929595f6695ce4dc65522c88837a6
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-07-09 17:07:37 +0900

    Bash: Solve ls coloring problem.
    * Use bat instead of less & syntax-highlight-less-pipe for syntax
      highlighting.
    * Alias ls, less, and open wisely and rc scripts reference them.
    * For mac, use exa and bat instead of (Gnu or BSD) ls and less.

--- bash/macrc/bsd.sh
+++ bash/macrc/bsd.sh
@@ -2,8 +2,8 @@ alias diff="diff --ignore-space-change"
 
 # if ls --version > /dev/null; then
   # ls Gnu coreutils
-  alias ls="ls --color=auto -p"
-  export L_LS_COMMAND='ls --color=auto -p'
+  # alias ls="ls --color=auto -p"
+  # export L_LS_COMMAND='ls --color=auto -p'
 # else
 #   alias ls="ls -p"
 #   export L_LS_COMMAND='ls -p'
