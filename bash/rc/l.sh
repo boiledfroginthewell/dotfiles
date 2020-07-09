50affc5d486774a0d47cd9a3d1dfa51e5792596f
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-07-09 17:07:37 +0900

    Bash: Solve ls coloring problem.
    * Use bat instead of less & syntax-highlight-less-pipe for syntax
      highlighting.
    * Alias ls, less, and open wisely and rc scripts reference them.
    * For mac, use exa and bat instead of (Gnu or BSD) ls and less.

--- bash/rc/l.sh
+++ bash/rc/l.sh
@@ -1,5 +1,4 @@
-if [[ "$OSTYPE" != "darwin"* ]] \
-	|| readlink -m . &> /dev/null; then
+if [[ "$OSTYPE" != "darwin"* ]] || readlink -m . &> /dev/null; then
 	# Gnu readlink (coreutils) is available
 	READLINK_COMMAND="readlink -m"
 else
@@ -8,29 +7,23 @@ else
 fi
 
 l() {
-local LESS_COMMAND=$(type lv > /dev/null 2>&1 && echo lv || echo less)
-local OPEN_COMMAND=$(type exo-open > /dev/null 2>&1 && echo exo-open || \
-	(type xdg-open > /dev/null 2>&1 && echo xdg-open ||  \
-	echo open))
-local LS="${L_LS_COMMAND:-ls --color=auto -p}"
+	local opt=()
+	while [ "${1:0:1}" = "-" ]; do
+		opt+=($1)
+		shift
+	done
 
-opt=""
-while [ "${1:0:1}" = "-" ]; do
-	opt=$opt' '"$1"
-	shift
-done
-
-# execute ls for directory and less for file
-if [ ! -t 0 ]; then
-	# read data from stdin
-	$LESS_COMMAND $opt
-elif [ -z "$1" ];then
-	$LS $opt $1
-elif [ -d "$1" ]; then
-	$LS $opt "$1"
-elif [ -n "$(file --mime "$($READLINK_COMMAND "$1")" | grep -e 'charset=binary' | grep -v x-empty)" ] ;then
-	$OPEN_COMMAND "$1"
-else
-	$LESS_COMMAND "$1"
-fi
+	# execute ls for directory and less for file
+	if [ ! -t 0 ]; then
+		# read data from stdin
+		${LESS_COMMAND:-less} "${opt[@]}"
+	elif [ -z "$1" ];then
+		${LS_COMMAND:-ls} "${opt[@]}"
+	elif [ -d "$1" ]; then
+		${LS_COMMAND:-ls} "${opt[@]}" "$1"
+	elif [ -n "$(file --mime "$($READLINK_COMMAND "$1")" | grep -e 'charset=binary' | grep -v x-empty)" ]; then
+		${OPEN_COMMAND:-open} "${opt[@]}" "$1"
+	else
+		${LESS_COMMAND:-less} "${opt[@]}" "$1"
+	fi
 }
