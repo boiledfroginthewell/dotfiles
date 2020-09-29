932dbd8d9d082b40810372596384666f90bfdea2
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-09-29 10:35:02 +0900

    Zsh: Configure zsh for mac.

--- zsh/macrc/fzf.mac.sh
+++ zsh/macrc/fzf.mac.sh
@@ -4,13 +4,18 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
+if [ -n "$ZSH_VERSION" ]; then
+  shell=zsh
+else
+  shell=bash
+fi
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.$shell" 2> /dev/null
 
 # Key bindings
 # ------------
-KEY_BINDING_FILE="/usr/local/opt/fzf/shell/key-bindings.bash"
+KEY_BINDING_FILE="/usr/local/opt/fzf/shell/key-bindings.$shell"
 # source <(< $KEY_BINDING_FILE sed 's/\C-t/\C-s/')
 source <(< $KEY_BINDING_FILE sed 's/.*bind .*C-t.*/:/')
 
