0651b03d533d16983f7d9e6f4540592afc9cb1a8
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-10-01 14:53:13 +0900

    Mac: mac.

--- zsh/macrc/bsd.sh
+++ zsh/macrc/bsd.sh
@@ -1,14 +1,4 @@
 alias diff="diff --ignore-space-change"
 
-# if ls --version > /dev/null; then
-  # ls Gnu coreutils
-  # alias ls="ls --color=auto -p"
-  # export L_LS_COMMAND='ls --color=auto -p'
-# else
-#   alias ls="ls -p"
-#   export L_LS_COMMAND='ls -p'
-#   export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
-# fi
-
 export CLICOLOR=1
 
--- zsh/macrc/mac.sh
+++ zsh/macrc/mac.sh
@@ -3,3 +3,5 @@ export FIGNORE="$FIGNORE:DS_Store"
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
 
+PATH="/Applications/MacVim.app/Contents/bin/:$PATH"
+
