47c597898bfd8e275b2c2fe521ab613c396d2b93
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2020-11-16 16:06:37 +0900

    Xkeysnail: Prefer Key.* over K("*").

--- keymap/xkeysnail/config.py
+++ keymap/xkeysnail/config.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import re
-from xkeysnail.transform import *
 from xkeysnail.transform import *
 
 
@@ -88,47 +86,62 @@ logical_shift_layer: bool = False
 define_conditional_modmap(
 	lambda wm_class, device_name: logical_shift_layer,
 	{
-		K("KEY_1"): Key.F1,
-		K("KEY_2"): Key.F2,
-		K("KEY_3"): Key.F3,
-		K("KEY_4"): Key.F4,
-		K("KEY_5"): Key.F5,
-		K("KEY_6"): Key.F6,
-		K("KEY_7"): Key.F7,
-		K("KEY_8"): Key.F8,
-		K("KEY_9"): Key.F9,
-		K("KEY_0"): Key.F10,
-		K("MINUS"): Key.F11,
-		K("EQUAL"): Key.F12,
+		Key.KEY_1: Key.F1,
+		Key.KEY_2: Key.F2,
+		Key.KEY_3: Key.F3,
+		Key.KEY_4: Key.F4,
+		Key.KEY_5: Key.F5,
+		Key.KEY_6: Key.F6,
+		Key.KEY_7: Key.F7,
+		Key.KEY_8: Key.F8,
+		Key.KEY_9: Key.F9,
+		Key.KEY_0: Key.F10,
+		Key.MINUS: Key.F11,
+		Key.EQUAL: Key.F12,
+
+		# 1st row
+		Key.Q: Key.ESC,
+		Key.W: Key.DELETE,
+		Key.E: Key.HOME,
+		Key.R: Key.END,
+		# Key.T: K("y"),
+
+		# Key.Y: K("f"),
+		# Key.U: K("g"),
+		Key.I: Key.ENTER,
+		Key.O: Key.ENTER,
+		# Key.P: K("l"),
+		# Key.LEFT_BRACE: Key.SLASH,
+		# Key.RIGHT_BRACE: Key.LEFT_BRACE,
 
 		# 2nd row
-		K("a"): K("C-a"),
-		K("s"): K("C-o"),
-		K("d"): K("e"),
-		K("f"): K("u"),
-		K("g"): K("i"),
+		Key.A: K("C-a"),
+		Key.S: K("C-o"),
+		Key.D: Key.PAGE_UP,
+		Key.F: Key.PAGE_DOWN,
+		# Key.G: K("i"),
 
-		K("h"): Key.LEFT,
-		K("j"): Key.UP,
-		K("k"): Key.DOWN,
-		K("l"): Key.RIGHT,
-		K("SEMICOLON"): K("s"),
-		K("APOSTROPHE"): Key.MINUS,
-		K("GRAVE"): Key.RIGHT_BRACE,
+		Key.H: Key.LEFT,
+		Key.J: Key.UP,
+		Key.K: Key.DOWN,
+		Key.L: Key.RIGHT,
+		# Key.SEMICOLON: K("s"),
+		# Key.APOSTROPHE: Key.MINUS,
+		# Key.GRAVE: Key.RIGHT_BRACE,
 
 		# 3rd row
-		# K("z"): Key.SEMICOLON,
-		# K("x"): K("q"),
-		# K("c"): K("j"),
-		# K("v"): K("k"),
-		# K("b"): K("x"),
-		# 
-		# K("n"): K("b"),
-		# K("m"): K("m"),
-		# K("COMMA"): K("w"),
-		# K("DOT"): K("v"),
-		# K("SLASH"): K("z"),
-		# # Key.BACKSLASH: Key.BACKSLASH,
+		Key.Z: K("C-z"),
+		Key.X: K("C-x"),
+		Key.C: K("C-c"),
+		Key.V: K("C-v"),
+		# Key.B: K("x"),
+
+		# Key.N: K("b"),
+		# Key.M: K("m"),
+		# Key.COMMA: K("w"),
+		# Key.DOT: K("v"),
+		# Key.SLASH: K("z"),
+		# Key.BACKSLASH: Key.BACKSLASH,
 	}
 )
 
