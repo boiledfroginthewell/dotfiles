e9249e93bded74976b88f1d7685377e4de1ca898
boiledfroginthewell <boiledfroginthewell@github.com>
2021-03-25 19:46:19 +0900

    xkeysnail: make xkeysnail service

--- keymap/xkeysnail/run.sh
+++ keymap/xkeysnail/run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then
-	echo "Error: Root priviledge is required to run" >&2
-	exit 1
-fi
-
 CDIR=$(dirname $(readlink -f $0))
-
-pkill xcape
-trap "$CDIR/../xmodmap/xcape.sh; exit" INT QUIT TERM EXIT
-
 $HOME/opt/pyenv/versions/3.8.5/bin/python3.8 "$CDIR/xkeysnail/bin/xkeysnail" --quiet "$CDIR/config.py"
 
