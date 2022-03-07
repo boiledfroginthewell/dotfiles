33acf9172e393446803b3404e3e4af2be1cf6ad2
boiledfroginthewell <boiledfroginthewell@github.com>
2022-03-07 22:52:24 +0900

    vscode: add cheatsheets

--- vscode/.install.sh
+++ vscode/.install.sh
@@ -15,6 +15,7 @@ if [ -e "$VSCODE_CONF_DIR" ]; then
 	ln -s "$CDIR/settings.json" "$VSCODE_CONF_DIR/"
 	ln -s "$CDIR/snippets" "$VSCODE_CONF_DIR/"
 	ln -sf "$CDIR/keybindings.json" "$VSCODE_CONF_DIR/"
+	ln -s "$CDIR/cheatsheats" "$HOME/.cheatsheets"
 	echo Installed VSCode config
 else
 	echo Skipping VSCode
