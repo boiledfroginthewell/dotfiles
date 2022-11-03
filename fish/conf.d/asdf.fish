d5bcb99568e5e8fc6ff21f6886005f302be2669d
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2022-11-03 22:14:22 +0900

    fish: improve asdf setting

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -3,4 +3,5 @@ not status -i || status -c && exit
 set -q ASDF_DIR || set -gx ASDF_DIR "$HOME/.asdf"
 set -gx ASDF_DATA_DIR "$XDG_DATA_HOME/asdf"
 source "$ASDF_DIR"/asdf.fish
+set -p fish_complete_path "$ASDF_DIR/completions"
 
