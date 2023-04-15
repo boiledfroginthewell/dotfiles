fa5f6133e3082df4953f68419149e2602b708c15
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2023-04-15 21:51:36 +0900

    python: remove flake8 that does not support global config

--- python/.install.sh
+++ python/.install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 CDIR=$(cd $(dirname $0); pwd)
-# ln -s $CDIR/pylintrc ~/.config/pylintrc
-ln -s $CDIR/flake8 "$XDG_CONFIG_HOME/flake8"
+ln -s $CDIR/pylintrc ~/.config/pylintrc
 
