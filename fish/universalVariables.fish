4bf3ad274b7623d6c168f8e1d4c6a3059001d631
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2023-11-05 19:39:27 +0900

    fish: make go follow XDG Desktop rules

--- fish/universalVariables.fish
+++ fish/universalVariables.fish
@@ -5,6 +5,7 @@ set -Ux RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/ripgreprc
 set -Ux DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 set -U fisher_path $XDG_DATA_HOME/fisher
 
+set -Ux GOPATH $XDG_CACHE_HOME/go
 set -Ux VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # set -U CONFIG_MODE job
