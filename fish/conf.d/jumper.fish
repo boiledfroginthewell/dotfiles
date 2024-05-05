6a2813f05dc076fc2af7c3e943b840495188b90f
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2024-05-05 21:14:04 +0900

    jumper: migrate from z to jumper

---
+++ fish/conf.d/jumper.fish
@@ -0,0 +1,8 @@
+not status -i || status -c && exit
+
+set -gx __JUMPER_FOLDERS $XDG_DATA_HOME/jumper/jfolders
+set -gx __JUMPER_FILES $XDG_DATA_HOME/jumper/jfiles
+
+sed 's;bind \\\\cu;# ;' < /opt/jumper/shell/jumper.fish \
+| source
+
