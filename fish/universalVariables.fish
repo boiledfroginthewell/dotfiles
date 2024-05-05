04c7c4e80ab2b4d96e53f13bb069d80d6d54ef82
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2024-05-05 21:06:34 +0900

    systemctl: add user-level environment variables

--- fish/universalVariables.fish
+++ fish/universalVariables.fish
@@ -1,10 +1,7 @@
 #!/usr/bin/env fish
 
 set -U fish_greeting ""
-set -Ux RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/ripgreprc
-set -Ux DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 set -U fisher_path $XDG_DATA_HOME/fisher
 
-set -Ux GOPATH $XDG_CACHE_HOME/go
 set -Ux VIRTUAL_ENV_DISABLE_PROMPT 1
 
