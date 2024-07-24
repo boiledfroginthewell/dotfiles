2361427b4faebcccc12c14e000559106b6bae5eb
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2024-07-24 14:21:36 +0900

    nvim: support local plugin config

--- nvim/after/ftplugin/yaml.vim
+++ nvim/after/ftplugin/yaml.vim
@@ -3,3 +3,6 @@ set shiftwidth=2
 set expandtab
 set iskeyword+=-
 set indentkeys-=-
+
+hi link @punctuation.delimiter.yaml Normal
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -108,6 +108,11 @@ local config = {
 		end,
 	},
 
+	-- A vim plugin to perform diffs on blocks of code
+	{ 'AndrewRadev/linediff.vim',
+		lazy = false,
+	},
+
 	{ "lewis6991/gitsigns.nvim",
 		opts = {
 			-- signcolumn = true
@@ -891,17 +896,6 @@ local config = {
 
 	-- Language specific plugins
 	------------------------------
-	{
-		'MeanderingProgrammer/markdown.nvim',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter', -- Mandatory
-			'nvim-tree/nvim-web-devicons', -- Optional but recommended
-		},
-		config = function()
-			require('render-markdown').setup({})
-		end,
-	},
-
 	-- Vim editing support for kmonad config files
 	{ 'kmonad/kmonad-vim',
 		ft = 'kbd',
@@ -933,6 +927,10 @@ local config = {
 		},
 	},
 }
+local ok, localConfig = pcall(require, 'local_plugins')
+if ok then
+	config = vim.list_extend(config, localConfig)
+end
 require("lazy").setup(config)
 
 -- vim.cmd[[colorscheme desert]]
