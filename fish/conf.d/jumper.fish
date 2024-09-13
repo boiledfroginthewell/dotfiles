141bb49982fce46ce321fe3cb0ea6a3a4ceb283d
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2024-09-13 14:40:58 +0900

    fish: upgrade jumper config

--- fish/conf.d/jumper.fish
+++ fish/conf.d/jumper.fish
@@ -3,6 +3,5 @@ not status -i || status -c && exit
 set -gx __JUMPER_FOLDERS $XDG_DATA_HOME/jumper/jfolders
 set -gx __JUMPER_FILES $XDG_DATA_HOME/jumper/jfiles
 
-sed 's;bind \\\\cu;# ;' < (nvl -f {,$HOME}/opt/jumper/shell/jumper.fish) \
-| source
+jumper shell fish | sed 's;bind \\\\cu;# ;' | source
 
