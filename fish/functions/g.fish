3fc34e67803459cc6444a559d7664eb5911ed106
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2024-09-27 18:06:52 +0900

    fish: improve g

--- fish/functions/g.fish
+++ fish/functions/g.fish
@@ -6,7 +6,16 @@ function g --wrap $_GIT
 	else if [ "$argv[1]" = "-" ]
 		$_GIT switch $argv
 	else if [ "$argv[1]" = "cd" ]
-		set worktree (g worktree list | fzf | choose 0)
+		set worktrees (git worktree list)
+		if [ (count $worktrees) = 2 ]
+			cd (
+				printf "%s\n" $worktrees \
+				| cut -d " " -f 1 \
+				| grep -v (git rev-parse --show-toplevel)
+			)
+			return
+		end
+		set worktree (printf "%s\n" $worktrees | fzf | choose 0)
 		set -q worktree && cd "$worktree"
 	else if [ "$argv[1]" = "addi" ]
 		if type forgit::add &> /dev/null
@@ -16,6 +25,9 @@ function g --wrap $_GIT
 		end
 	else if [ "$argv[1]" = "pr" ] && [ "$argv[2]" = "switch" ]
 		$_GIT pr checkout $argv[3..]
+	else if [ "$argv[1]" = "switch" ] && [ (count $argv) = 1 ]
+		set branch ($_GIT branch -a --color | fzf | cut -c 3-)
+		set -q branch && g switch "$branch"
 	else
 		$_GIT $argv
 	end
