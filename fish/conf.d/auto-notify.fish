bfc00d189d4721cb64b5551d913d8544f31d3bbd
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2024-12-15 20:30:27 +0900

    fish: improve

--- fish/conf.d/auto-notify.fish
+++ fish/conf.d/auto-notify.fish
@@ -2,16 +2,19 @@ not status -i || status -c && exit
 
 set NOTIFY_THREASHOLD 60000
 if not set -q AUTO_NOTIFY_EXCLUDE_PATTERNS
-	set AUTO_NOTIFY_EXCLUDE_PATTERNS l 'gg?' diff watchexec fzf 'm[ae]n' lazygit vi
+	set AUTO_NOTIFY_EXCLUDE_PATTERNS l 'gg?' diff watchexec fzf 'm[ae]n' lazygit vi?
 end
 set NOTIFY_EXCLUDE '^('(string join '|' $AUTO_NOTIFY_EXCLUDE_PATTERNS)')( |$)'
 
 function __check_time -e fish_postexec
-	if [ $CMD_DURATION -gt 60000 ]
+	if [ $status != 0 ]
+		set cmdStatus "❌ "
+	end
+	if [ $CMD_DURATION -gt $NOTIFY_THREASHOLD ]
 		if string match -r "$NOTIFY_EXCLUDE" "$argv"
 			return
 		end
-		set body "⏱️$(printf '%.0f' (math $CMD_DURATION / 60)) s\n> $argv"
+		set body "$cmdStatus⏱️$(printf '%.0f' (math $CMD_DURATION / 60)) s\n> $argv"
 		if type -q notify-send
 			notify-send -a "Fish" -i "~/images/fish.png" "Command Notification" "$body"
 		else
