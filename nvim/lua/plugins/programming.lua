e4fb898472c627c3a406bbac62ed06eebd9cdc7b
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2025-02-24 19:57:48 +0900

    nvim: fix dial setting

--- nvim/lua/plugins/programming.lua
+++ nvim/lua/plugins/programming.lua
@@ -139,6 +139,18 @@ return {
 			{ "<C-a>", "<Plug>(dial-increment)", mode = { "n", "v" } },
 			{ "<C-x>", "<Plug>(dial-decrement)", mode = { "n", "v" } },
 		},
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group{
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.constant.alias.bool,
+					augend.semver.alias.semver,
+				},
+			}
+		end,
 	},
 
 	{'Exafunction/codeium.vim',
