81e1fe419d4d53f6edc2cc422c068840ef7aa22c
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2025-03-19 18:40:05 +0900

    wezterm: adjust font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -27,7 +27,7 @@ config.default_cursor_style = "SteadyBar"
 
 -- config.color_scheme = 'Builtin Dark'
 config.color_scheme = 'Andromeda'
-config.font_size = 13.5
+config.font_size = 13.8
 config.colors = {
 	split = "#4444AA",
 	compose_cursor = "#000099",
