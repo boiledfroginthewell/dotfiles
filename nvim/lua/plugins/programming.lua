59ab25eac4005351a0632c257837ebd661301051
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2025-03-20 18:10:46 +0900

    nvim: add nmiv-lint

--- nvim/lua/plugins/programming.lua
+++ nvim/lua/plugins/programming.lua
@@ -139,6 +139,20 @@ return {
 		end,
 	},
 
+	{ 'mfussenegger/nvim-lint',
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				python = { "mypy" },
+			}
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+
 	{'Exafunction/codeium.vim',
 		event = 'BufEnter',
 		config = function ()
