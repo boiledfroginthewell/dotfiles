89fe900bcd463bc0a9a9afb760accb4e47366e7c
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2025-09-20 15:58:15 +0900

    nvim: improve markdown rendering

---
+++ nvim/queries/markdown_inline/highlights.scm
@@ -0,0 +1,7 @@
+; https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/331#issuecomment-2654843484
+;; extends
+
+((backslash_escape)
+  @backslash
+  (#offset! @backslash 0 0 0 -1)
+  (#set! conceal ""))
