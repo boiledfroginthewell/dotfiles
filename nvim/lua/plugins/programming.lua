2d639b5491065efa7a8c892940c818b64f2b35ce
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2026-04-26 16:53:55 +0900

    nvim: add toggleterm lazygit

--- nvim/lua/plugins/programming.lua
+++ nvim/lua/plugins/programming.lua
@@ -45,7 +45,32 @@ local spec = {
 		-- order to load the plugin when the command is run for the first time
 		keys = {
 			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-		}
+		},
+		enabled = false,
+	},
+
+	-- A neovim lua plugin to help easily manage multiple terminal windows
+	{'akinsho/toggleterm.nvim',
+		version = "*",
+		config = true,
+		keys = {
+			{
+				"<M-g>",
+				function()
+					if not _TOGGLETERM_LAZYGIT then
+						local Terminal = require("toggleterm.terminal").Terminal
+						_TOGGLETERM_LAZYGIT = Terminal:new({
+							cmd = "lazygit",
+							hidden = true,
+							direction = "tab",
+						})
+					end
+					_TOGGLETERM_LAZYGIT:toggle()
+				end,
+				desc = "LazyGit",
+				mode = {"n", "t"},
+			},
+		},
 	},
 
 	{ "tpope/vim-fugitive",
