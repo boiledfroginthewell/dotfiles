a78b03550b2b0a2139dc3faf202282b740493393
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2026-05-02 13:35:59 +0900

    nvim: upgrade fff config

--- nvim/lua/plugins/basic-editing.lua
+++ nvim/lua/plugins/basic-editing.lua
@@ -591,21 +591,26 @@ return {
 	{
 		'dmtrKovalenko/fff.nvim',
 		build = function()
-			-- this will download prebuild binary or try to use existing rustup toolchain to build from source
-			-- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+			-- downloads a prebuilt binary or falls back to cargo build
 			require("fff.download").download_or_build_binary()
 		end,
-		opts = { -- (optional)
+		opts = {
 			layout = {
 				prompt_position = "top",
+				flex = {
+					wrap = "bottom"
+				},
 			},
 			keymaps = {
 				preview_scroll_up = '<PageUp>',
 				preview_scroll_down = '<PageDown>',
 			},
+			git = {
+				status_text_color = true,
+			},
 			debug = {
-				enabled = true,     -- we expect your collaboration at least during the beta
-				show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+				enabled = false,
+				show_scores = true,
 			},
 		},
 		-- No need to lazy-load with lazy.nvim.
@@ -616,7 +621,7 @@ return {
 				",,", -- try it if you didn't it is a banger keybinding for a picker
 				function() require('fff').find_files() end,
 				desc = 'FFFind files',
-			}
+			},
 		}
 	},
 
