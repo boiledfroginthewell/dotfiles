8e3ba5a2e5fd0619e5cc1bb98af0d20a5f4adb50
boiledfroginthewell <boiledfroginthewell@users.noreply.github.com>
2026-05-22 18:29:42 +0900

    nvim: fix config

--- nvim/lua/plugins/basic-editing.lua
+++ nvim/lua/plugins/basic-editing.lua
@@ -136,8 +136,12 @@ return {
 		event = { 'BufReadPre', 'BufNewFile' },
 		opts = {
 			add_default_keybindings = false,
+			bookmarks = {},
 		},
 		config = function(_, opts)
+			for i = 0, 9 do
+				table.insert(opts.bookmarks, { sign = tostring(i)  })
+			end
 			require("markit").setup(opts)
 			vim.api.nvim_set_hl(0, "MarkSignLineHL", { bg = "#106010" })
 		end,
